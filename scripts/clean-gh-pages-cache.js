8e99e01220a92765ceea47c881383cdf30b5b965
AnanMoktan <89403086+AnanMoktan@users.noreply.github.com>
2025-07-30 20:53:16 -0500

    Deploy production build

---
+++ scripts/clean-gh-pages-cache.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+
+const cacheDir = path.join(
+  __dirname,
+  "..",
+  "node_modules",
+  ".cache",
+  "gh-pages"
+);
+
+function deleteFolderRecursive(folderPath) {
+  if (fs.existsSync(folderPath)) {
+    fs.readdirSync(folderPath).forEach((file) => {
+      const curPath = path.join(folderPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(folderPath);
+    console.log(`Deleted cache directory: ${folderPath}`);
+  } else {
+    console.log(`Cache directory does not exist: ${folderPath}`);
+  }
+}
+
+deleteFolderRecursive(cacheDir);
